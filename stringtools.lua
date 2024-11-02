998dba18ed7887cb0a897707b7928fe469eed5b3
Orion <90088721+banana-boye@users.noreply.github.com>
2024-11-02 13:19:45 +0100

    test

--- stringtools.lua
+++ stringtools.lua
@@ -1,7 +1,7 @@
 --[[
 MIT License
 
-Copyright (c) 2024 Orion
+Copyright (c) 2024 Oriont
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
